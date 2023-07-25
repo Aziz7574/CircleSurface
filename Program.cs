d57949de0c7c818d935cd398ea1f04d23355e069
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2023-07-25 11:13:47 +0500

    Check-version-control

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
-﻿
+﻿using System;
 
-using System;
-
-namespace Space{
-    public class Program{
-        public static void Main(string[] args){
+namespace Space
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             Console.WriteLine("Enter the radius of circle");
 
             float radius = Convert.ToInt32(Console.ReadLine());
             const float p = 3.14F;
-            
-            Console.WriteLine("Calculating the surface of circle");
 
-            Console.WriteLine($"Area of circle is {p * radius * radius}");
+            if (radius > 0)
+            {
+                Console.WriteLine("Calculating the surface of circle");
+                Console.WriteLine($"Area of circle is {p * radius * radius}");
+            }
+
+            else if (radius == 0)
+            {
+                Console.WriteLine("Radius can't be null");
+            }
+            else
+            {
+                Console.WriteLine("Radius can't be lower than null");
+            }
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Circle Surface
 
 
- [Circle Surface](circle-and-spheres-with-mathematical-formulas-of-circumference-area-EG44RF.jpg)
+ ![Circle Surface](./Assets/circle-and-spheres-with-mathematical-formulas-of-circumference-area-EG44RF.jpg)
+
+
+
+> ## Steps to calculate the  _surface_  of circle
+>>
+>>**Formula to calcualte the surface = Math.PI * r ^ 2**
+>>
+>> **Show the results**
+>>
+>> **That is it**
+
+
+
+
+
+
+ 
